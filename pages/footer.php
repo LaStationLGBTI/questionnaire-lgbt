b5cac480d85510bd78c2dfb70adfd0facac3385a
Emmy D'Anello <emmy@luemy.eu>
2025-02-03 21:17:18 +0100

    Fix footer
    Signed-off-by: Emmy D'Anello <emmy@luemy.eu>

---
+++ pages/footer.php
@@ -0,0 +1,10 @@
+<footer class="u-align-center u-clearfix u-container-align-center" id="sec-33ba" >
+  <div class="u-container-style u-expanded-width u-grey-70 u-group" style="background-color:##7d63a4;">
+    <div style="display: flex;  align-items: center;" class="u-container-layout u-container-layout-1">
+      <p style="margin:0; font-size:32px; color: #b480ff;" class="u-text u-text-1"> La Station LGBTQIA+</p>
+      <img src="/images/station.jpg" alt="" style="width:100px; height:100px;">
+      <p style="margin-bottom:1em;margin-right:auto; margin-left:auto; margin-top:auto; text-align:center;  font-size:12px;">Conception de la page : R. (Hex) ; maître de stage : Gérald Schlemminger, (c) 2025 La STATION / Luc Fey</p></pConception>
+      <img src="/images/college.jpg" alt="" style="width:250px; height:100px; margin-left: auto;">
+    </div>
+  </div>
+</footer>
