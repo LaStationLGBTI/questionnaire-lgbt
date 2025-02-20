8ca0f13350a27b4fb6657de0b872e2d7757dc48f
Schlemminger <schlemminger.gerald@gmail.com>
2025-02-20 23:41:25 +0100

    Add files via upload

---
+++ stats.php
@@ -0,0 +1,195 @@
+<!DOCTYPE html><?php session_start(); ?>
+<html style="font-size: 16px;" lang="fr">
+
+<head>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta charset="utf-8">
+	<title>Question</title>
+	<link rel="stylesheet" href="nicepage.css" media="screen">
+	<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+	<link rel="stylesheet" href="Question.css" media="screen">
+
+    <style>
+        .chart-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: center;
+            gap: 20px;
+            margin-top: 20px;
+        }
+
+        .chart-box {
+            width: 18em;
+            height: 30em;
+			margin-bottom: 3em;
+        }
+    </style>
+<body data-path-to-root="./" data-include-products="false" class="u-body u-xl-mode" data-lang="fr" style="height:100%">
+	<section  id="sec-089e">
+		<div class="u-container-style u-expanded-width u-grey-10 u-group u-group-1">
+			<div class="u-container-layout u-container-layout-1">
+				<div class="u-clearfix u-sheet u-sheet-1" style="text-align: center;">
+				<div id="chartsContainer" class="chart-container"></div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+    <script>
+        const chartInstances = {};
+        fetch('stats_getdata.php')
+            .then(response => response.json())
+            .then(data => {
+                data.formattedData.forEach((item, index) => {
+                    if (item.type === 'qcm' || item.type === 'echelle') {
+                        createPieChart(item.question, item.responses, item.id);
+                    } else if (item.type === 'mct') {
+                        createStackedBarChart(item.sub_questions, item.responses, item.id, item.question);
+                    } else if (item.type === 'lien') {
+                        createStackedBarChart(item.sub_questions, item.sub_responses, item.id, item.question);
+                    }
+                });
+
+                data.answers.forEach((item, index) => {
+
+                    let questionId = item.question;
+                    let responseId = item.response;
+                    let chart = chartInstances[parseInt(questionId)];
+                    if (!responseId) {
+                        if (chart) {
+                            let newSubResponses = item.subresponse.split(",");
+                            let newSubQuestion = item.subquestion.split(",");
+
+
+                            let dataRep = newSubResponses.map(Number);
+                            let dataQuest = newSubQuestion.map(Number);
+                            console.log(dataQuest);
+                            console.log(dataRep);
+                            console.log(questionId);
+                            dataQuest.forEach((questF, index) => {
+                                console.log(chart.data.datasets[dataRep[index] - 1].data[questF - 1]);
+                                chart.data.datasets[dataRep[index] - 1].data[questF - 1]++;
+                            });
+
+                            chart.update();
+                        }
+                    }
+                    else
+                        if (chart) {
+                            chart.data.datasets[0].data[responseId] += 1;
+                            chart.update();
+                        }
+
+
+                });
+            })
+            .catch(error => console.error('Error:', error));
+
+        function createPieChart(question, responses, chartIndex) {
+            const validResponses = responses.filter(response => response !== "null");
+            let container = document.getElementById("chartsContainer");
+            let div = document.createElement("div");
+            div.className = "chart-box";
+            let questionLabel = document.createElement("div");
+            questionLabel.textContent = `Question: ${question}`;
+            questionLabel.style.textAlign = "center";
+            div.appendChild(questionLabel);
+            let canvas = document.createElement("canvas");
+            canvas.id = "chart_" + chartIndex;
+            div.appendChild(canvas);
+            container.appendChild(div);
+            const chart = new Chart(canvas, {
+                type: 'pie',
+                data: {
+                    labels: [question, ...validResponses],
+                    datasets: [{
+                        data: [0, ...validResponses.map(() => 1)],
+                        backgroundColor: ["Blue", "#FF0080", "Yellow", "Orange", "Red"]
+                    }]
+                },
+                options: {
+                    plugins: {
+                        tooltip: {
+                            enabled: true
+                        }
+                    },
+                    interaction: {
+                        mode: 'nearest'
+                    },
+                    responsive: false,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    }
+                }
+            });
+            chartInstances[chartIndex] = chart;
+        }
+        function createStackedBarChart(subQuestions, responses, chartIndex, question) {
+            const validResponses = responses.filter(response => response !== "null");
+            let container = document.getElementById("chartsContainer");
+            let canvas = document.createElement("canvas");
+            canvas.id = "chart_" + chartIndex;
+            let div = document.createElement("div");
+            let questionLabel = document.createElement("div");
+            questionLabel.textContent = `Question: ${question}`;
+            questionLabel.style.textAlign = "center";
+            div.appendChild(questionLabel);
+            div.className = "chart-box";
+            div.appendChild(canvas);
+            
+            container.appendChild(div);
+            let datasets = responses.map((response, index) => ({
+                label: response.slice(0, 32) + "...",
+                data: response.slice(0, 32) === "null" ? subQuestions.map(() => 0) : subQuestions.map(() => 1),
+                backgroundColor: `hsl(${index * 137.508}, 70%, 50%)`
+            }));
+
+            const chart = new Chart(canvas, {
+                type: 'bar',
+                data: {
+                    labels: subQuestions,
+                    datasets: datasets
+                },
+                options: {
+
+                    scales: {
+                        x: { beginAtZero: true, stacked: true },
+                        y: {beginAtZero: true, stacked: true}
+                    },
+                    interaction: {
+                        mode: 'nearest'
+                    },
+                    responsive: true,
+                    maintainAspectRatio: false,
+                    plugins: {
+                        legend: {
+                            display: false
+                        }
+                    }
+                }
+            });
+            chartInstances[chartIndex] = chart;
+
+        }
+    </script>
+	<script>
+		setTimeout(() => {
+			const section = document.querySelector('section');
+			const newDiv = document.createElement('div');
+			newDiv.id = "footer-placeholder";
+			section.insertAdjacentElement('afterend', newDiv);
+			fetch('pages/footer.php')
+				.then(response => response.text())
+				.then(data => {
+					document.getElementById('footer-placeholder').innerHTML = data;
+				});
+		}, 100); 
+	</script>
+
+
+</body>
+
+</html>
---
+++ stats_getdata.php
@@ -0,0 +1,131 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+header('Content-Type: application/json');
+
+$dsn = "mysql:host=localhost;dbname=lastation;charset=utf8mb4";
+$username = "root";
+$password = "";
+try {
+    $pdo = new PDO($dsn, $username, $password, [
+        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+    ]);
+    $stmt = $pdo->prepare("DELETE FROM stationq1 WHERE id = :id");
+    $stmt->execute(['id' => 2]);
+    //$stmt = $pdo->prepare("TRUNCATE TABLE stationr2");
+    //$stmt->execute();
+    $stmt = $pdo->query("SELECT * FROM stationq1");
+    $questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $stmt = $pdo->query("SELECT * FROM stationr2");
+    $reponsesdb = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+    $formattedData = [];
+    foreach ($reponsesdb as $row) {
+        $responseString = $row['reponse'];
+        $responseString = str_replace('&amp;', '&', $responseString);
+        $parts = explode('__', $responseString);
+        $QuestionsR = [];
+        foreach ($parts as $part) {
+            if (strpos($part, '&&') !== false) {
+                $subParts = explode('&&', $part);
+
+                $mainQuestion = '';
+                $subQuestions = [];
+                $subResponses = [];
+                if (empty($mainQuestion)) {
+                    $mainQuestion = explode('@', $subParts[0])[1];
+                    $mainQuestion = (int) $mainQuestion;
+                }
+                foreach ($subParts as $subPart) {
+                    $subPartArray = explode('|', $subPart);
+
+                    if (count($subPartArray) === 2) {
+                        list($question, $response) = $subPartArray;
+                        $questionValue = substr($question, strpos($question, '@') + 1);
+                        $responseValue = substr($response, strpos($response, '@') + 1);
+                        $subQuestions[] = $questionValue;
+                        $subResponses[] = $responseValue;
+                    }
+                }
+
+                $QuestionsR[] = [
+                    'question' => $mainQuestion,
+                    'response' => null,
+                    'subquestion' => implode(',', $subQuestions),
+                    'subresponse' => implode(',', $subResponses)
+                ];
+            } else {
+                list($question, $response) = explode('||', $part);
+                $questionValue = (int) substr($question, strpos($question, '@') + 1);
+                $responseValue = (int) substr($response, strpos($response, '@') + 1);
+
+                $QuestionsR[] = [
+                    'question' => $questionValue,
+                    'response' => $responseValue
+                ];
+            }
+        }
+    }
+    foreach ($questions as $row) {
+        $qtype = $row['qtype'];
+        $qid = $row['id'];
+        $questionText = $row['question'];
+
+        if ($qtype === "qcm" || $qtype === "echelle") {
+            $responses = [];
+            for ($i = 1; $i <= 5; $i++) {
+                if (!empty($row["rep$i"])) {
+                    $responses[] = $row["rep$i"];
+                }
+            }
+
+            $formattedData[] = [
+                'id' => $qid,
+                'type' => 'qcm',
+                'question' => $questionText,
+                'responses' => $responses,
+
+            ];
+
+        } elseif ($qtype === "mct") {
+            $subQuestions = explode("--", $row['rep1']);
+            $responses = [];
+            for ($i = 2; $i <= 5; $i++) {
+                if (!empty($row["rep$i"])) {
+                    $responses[] = $row["rep$i"];
+                }
+            }
+
+            $formattedData[] = [
+                'id' => $qid,
+                'type' => 'mct',
+                'question' => $questionText,
+                'sub_questions' => $subQuestions,
+                'responses' => $responses,
+            ];
+
+        } elseif ($qtype === "lien") {
+            $subQuestions = explode("--", $row['rep1']);
+            $subResponses = explode("--", $row['rep2']);
+
+            $formattedData[] = [
+                'id' => $qid,
+                'type' => 'lien',
+                'question' => $questionText,
+                'sub_questions' => $subQuestions,
+                'sub_responses' => $subResponses,
+            ];
+        }
+    }
+    $response = [
+        "formattedData" => $formattedData,
+        "answers" => isset($QuestionsR) ? $QuestionsR : []
+    ];
+    echo json_encode($response);
+
+} catch (PDOException $e) {
+    echo json_encode(['error' => $e->getMessage()]);
+}
+?>
