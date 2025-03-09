b5c1356cc947921b7e30a05de7a01cabe05420f0
Schlemminger <schlemminger.gerald@gmail.com>
2025-03-09 20:14:15 +0100

    Update index.php
    bd mis a jour

--- index.php
+++ index.php
@@ -657,6 +657,20 @@
 			} catch (PDOException $e) {
 				echo "Erreur connection: " . $e->getMessage();
 			}
+			$stmt = $conn->prepare("UPDATE stationq1 SET question = 'Où avez-vous entendu parler du sigle LGBTI+ / LGBTQIA+ ?' WHERE id = 3;");
+			$stmt->execute();
+			$stmt = $conn->prepare("UPDATE stationq1 SET question = 'Comment peut-on définir le harcèlement LGBTQIA+phobe envers les personnes LGBTQIA+ ?' WHERE id = 8;");
+			$stmt->execute();		
+			$stmt = $conn->prepare("UPDATE stationq1 SET question = 'Quel exemple illustre une forme de harcèlement LGBTQIA+phobe envers un·e jeune LGBTQIA+ à l'école ?' WHERE id = 9;");
+			$stmt->execute();
+			$stmt = $conn->prepare("UPDATE stationq1 SET question = 'Envers qui vous tourneriez-vous pour en parler du harcèlement LGBTQIA+phobe envers les personnes LGBTQIA+ ?' WHERE id = 11;");
+			$stmt->execute();				
+			$stmt = $conn->prepare("UPDATE stationq1 SET question = 'Connaissez-vous quelqu’un qui a subi un harcèlement LGBTQIA+phobe ?' WHERE id = 12;");
+			$stmt->execute();
+			$stmt = $conn->prepare("UPDATE stationq1 SET question = 'Où le harcèlement LGBTQIA+phobe (envers toi / envers d’autres) s’est-il produit ?' WHERE id = 13;");
+			$stmt->execute();
+			$stmt = $conn->prepare("ALTER TABLE stationq1 MODIFY question VARCHAR(100);");
+			$stmt->execute();				
 			$stmt = $conn->prepare("SELECT * FROM stationq1 WHERE level = 101 ORDER BY `id` ASC");
 
 			$stmt->execute();
