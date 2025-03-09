73b2724c598efdd116570b1fcca8cc51b462aecd
Schlemminger <schlemminger.gerald@gmail.com>
2025-03-09 21:05:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -674,8 +674,8 @@ session_start();
 			$stmt->execute();
 			$stmt = $conn->prepare("UPDATE stationq1 SET question = 'Où le harcèlement LGBTQIA+phobe (envers toi / envers d’autres) s\’est-il produit ?' WHERE id = 13;");
 			$stmt->execute();
-			$stmt = $conn->prepare("ALTER TABLE stationq1 MODIFY question VARCHAR(100);");
-			$stmt->execute();				
+			$stmt = $conn->prepare("ALTER TABLE stationq1 MODIFY COLUMN question VARCHAR(100)");
+			$stmt->execute();			
 			$stmt = $conn->prepare("SELECT * FROM stationq1 WHERE level = 101 ORDER BY `id` ASC");
 
 			$stmt->execute();
