4dddd08b75a6647cc27d337d12bb00258c4ec263
Schlemminger <schlemminger.gerald@gmail.com>
2025-03-09 21:12:25 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,8 +1,5 @@
 <!DOCTYPE html><?php 
 ini_set('session.gc_maxlifetime', 31536000);
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
 session_start(); 
 ?>
 <?php require_once 'conf.php'; ?>
