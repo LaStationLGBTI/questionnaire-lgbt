fc6bed202ebd91ba39a2f0866d835ef3fbc910f4
Schlemminger <schlemminger.gerald@gmail.com>
2025-03-24 13:04:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,11 @@
 <!DOCTYPE html><?php 
+ini_set('display_errors', 1);
+ 
+
+ini_set('display_startup_errors', 1);
+ 
+
+error_reporting(E_ALL);
 ini_set('session.gc_maxlifetime', 31536000);
 session_start(); 
 var_dump($_SESSION);
